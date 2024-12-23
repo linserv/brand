84efa2163c75d7cb8089c861ed28efdad3c7bb02
Riyaj Pathan <rjpathan19@gmail.com>
2024-12-23 09:43:54 +0530

    [FIX] product_brand: fixed test error by creating product & supplier record in test file instead of referencing from demo data file

--- product_brand/tests/common.py
+++ product_brand/tests/common.py
@@ -13,8 +13,25 @@ class CommonCase(TransactionCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
-        cls.product = cls.env.ref("product.product_product_4")
-        cls.supplier = cls.env.ref("base.res_partner_2")
+        cls.product = cls.env["product.product"].create({
+            "name": "Test Product",
+            "type": "product",
+            "default_code": "TEST_0001",
+            "standard_price": 500.0,
+            "weight": 0.01,
+        })
+        cls.supplier = cls.env["res.partner"].create({
+            "name": "Test supplier",
+            "is_company": True,
+            "street": "77 Santa Barbara Rd",
+            "city": "Pleasant Hill",
+            "state_id": cls.env.ref("base.state_us_5").id,
+            "country_id": cls.env.ref("base.us").id,
+            "zip": "94523",
+            "email": "test_supplier@yourcompany.example.com",
+            "phone": "(603)-996-3829",
+            "vat": "US12345673",
+        })
         cls.product_brand_obj = cls.env["product.brand"]
         cls.product_brand = cls.product_brand_obj.create(
             {
